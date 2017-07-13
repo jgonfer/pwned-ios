069e0279d56e850387ba18954e9357af2b9d61ea
j_gonfer <josep.gonfer@gmail.com>
2017-07-13 22:27:14 +0200

    Initial project

---
+++ pwned/pwned/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  pwned
+//
+//  Created by Josep Gonzalez Fernandez on 13/7/17.
+//  Copyright © 2017 Dreams Corner. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
---
+++ pwned/pwned/SearchViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SearchViewController.swift
+//  pwned
+//
+//  Created by Josep Gonzalez Fernandez on 13/7/17.
+//  Copyright © 2017 Dreams Corner. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
